6dbcd509cd4b20e5e3b54366c53a766d222e9df8
MrTequila <igi444@gmail.com>
2017-10-16 21:22:25 +0200

    Initial

---
+++ src/main/java/com/MrTequila/BookWorm/BookWormApplication.java
@@ -0,0 +1,12 @@
+package com.MrTequila.BookWorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookWormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookWormApplication.class, args);
+	}
+}
